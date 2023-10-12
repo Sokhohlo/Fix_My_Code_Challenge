7f320258347b1a1d5e82a820c6a9206bdb85edc7
Sokhohlo <122815158+Sokhohlo@users.noreply.github.com>
2023-10-12 11:51:15 +0200

    Add files via upload

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
 
-class square():
-    width = 0
-    height = 0
+# Editting 1: Make the class Square not square
+class Square():
+    # Editing 2: Adding Doc
+    """ start the class"""
 
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    # Edit 3: We don't need kwargs, we just will use width and height
+    def __init__(self, width, height):
+    # Edit 4: Adding doc
+        """ init the class"""
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        # Edit 5: adding doc
+        """ permmiter of the square"""
+        # Edit 6: Exchange the permiter of rectangle by the square
+        return 4 * self.width
 
     def __str__(self):
+        # Edit 7: Adding doc
+        """ String representation """
         return "{}/{}".format(self.width, self.height)
-
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
